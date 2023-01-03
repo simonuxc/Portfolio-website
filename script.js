4b498c2f4a8bf7121424a663a23ed8af431fcce1
Simonas Surdokas <simasurdokas@gmail.com>
2023-01-03 10:43:42 +0200

    Disabled scrolling when pic enlarged

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 var backdrop = document.getElementById("backdrop");
 var enlargedPicture = document.getElementById("enlarged-picture");
-console.log(enlargedPicture.src);
+var body = document.getElementsByTagName("body")[0];
+
 backdrop.addEventListener("click", function(e) {
     backdrop.style.display = "none";
     enlargedPicture.style.display = "none";
+    body.style.overflow = "visible";
 })
 
 document.addEventListener("click", function(e) {
     if (e.target.matches('.project-gallery__picture img')) {
         var picture = e.target
         enlargedPicture.src = picture.src;
-        console.log(picture.width);
-        console.log(picture.height);
         backdrop.style.display = "block";
         enlargedPicture.style.display = "block";
+        body.style.overflow = "hidden";
     }
 })
